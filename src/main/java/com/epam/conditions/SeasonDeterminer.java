a559deb0d9f91576d45ea9e037bc8e35201e3c26
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 10:34:20 +0500

    Update SeasonDeterminer.java

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,23 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        if (number == 1 || number ==2 || number ==12) {
+            System.out.println("Winter");
+        }
+        else if (number == 3 || number == 4 || number == 5) {
+            System.out.println("Spring");
+        }
+        else if (number == 6 || number == 7 || number == 8) {
+            System.out.println("Summer");
+        }
+        else if (number == 9 || number == 10 || number == 11) {
+            System.out.println("Autumn");
+        }
+        else System.out.println("Wrong month number");
+    }
+    public static void main(String[] args) {
+        SeasonDeterminer s = new SeasonDeterminer();
+        s.tellSeason(10);
     }
 
 }
