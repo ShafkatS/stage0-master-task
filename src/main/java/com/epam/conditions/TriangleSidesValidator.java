524f776a56c2637d3c134e1e927eda9996561c77
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 10:35:33 +0500

    Update TriangleSidesValidator.java

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,17 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        number = firstSide + secondSide + thirdSide;
+        if (number == 180 && firstSide != 0 && secondSide != 0 && thirdSide != 0) {
+            System.out.println("this is a valid triangle");
+        }
+        else
+            System.out.println("it's not a triangle");
 
+    }
+    public static void main(String[] args) {
+        TriangleSidesValidator t = new TriangleSidesValidator();
+        t.validate(80,40,60);
     }
 
 }
