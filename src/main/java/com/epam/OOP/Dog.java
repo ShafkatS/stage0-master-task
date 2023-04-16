201e996970744d6809ba246da1195912fb25e758
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 10:23:43 +0500

    Update Dog.java

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -1,5 +1,10 @@
 package com.epam.OOP;
 
-public class Dog {
+public class Dog extends  Animal{
+    public Dog() {
+    }
+    public Dog(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+    }
 
 }
