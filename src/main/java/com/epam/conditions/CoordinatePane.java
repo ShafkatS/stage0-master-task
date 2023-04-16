846e2fcf7d5c83dc00960ff27cca4892c146ed95
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 10:38:06 +0500

    Update CoordinatePane.java

--- src/main/java/com/epam/conditions/CoordinatePane.java
+++ src/main/java/com/epam/conditions/CoordinatePane.java
@@ -3,6 +3,25 @@ package com.epam.conditions;
 public class CoordinatePane {
 
     public void printQuadrant(int x, int y) {
+        if (x > 0 && y > 0)
+            System.out.println("First quadrant");
+
+        else if (x < 0 && y > 0)
+            System.out.println("Second quadrant");
+
+        else if (x < 0 && y < 0)
+            System.out.println("Third quadrant");
+
+        else if (x > 0 && y < 0)
+            System.out.println("Fourth quadrant");
+
+        else
+            System.out.println("Zero");
+    }
+    
+    public static void main(String[] args) {
+        CoordinatePane c = new CoordinatePane();
+        c.printQuadrant(0,5);
     }
 
 }
