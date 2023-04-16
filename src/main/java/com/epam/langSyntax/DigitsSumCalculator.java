5c149a3ec10c231a582078c5a315eb1787d48be2
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 20:08:16 +0500

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,10 +1,11 @@
 package com.epam.langSyntax;
 
 public class DigitsSumCalculator {
-     int sum = 0;
-     int value;
+   
 
     public void calculateSum(int number) {
+        int sum = 0;
+        int value;
         value = number % 10;
         sum = sum + value;
         number = number / 10;
