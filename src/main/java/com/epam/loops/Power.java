5b5acce9683891d3f2cfa71d4f4c1ab1f716555d
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 10:29:07 +0500

    Update Power.java

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,6 +3,15 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
+        
+         double result =1;
+        int i;
+
+        for(i = power; i >0; i--)
+        {
+            result *= numberToPrint;
+        }
+        System.out.println(numberToPrint + " in power " + power + " = " + result);
 
     }
 
