e2f69b780b9663fc7d52a0889e1ba0c2818f4e29
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 11:04:25 +0500

    Update Animal.java

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -45,12 +45,5 @@ public class Animal {
     }
   
   
-   public static void main(String[] args) {
-        Dog dog = new Dog("brown",4,true);
-        Bird bird = new Bird("blue",2,false);
-        System.out.println(dog.getDescription());
-        System.out.println(bird.getDescription());
-    }
-  
 
 }
