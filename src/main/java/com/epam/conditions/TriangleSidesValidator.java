18b30b8587076b47407f8fffcb0d08ad280d2af5
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 11:07:16 +0500

    Update TriangleSidesValidator.java

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -11,9 +11,6 @@ public class TriangleSidesValidator {
             System.out.println("it's not a triangle");
 
     }
-    public static void main(String[] args) {
-        TriangleSidesValidator t = new TriangleSidesValidator();
-        t.validate(80,40,60);
-    }
+
 
 }
