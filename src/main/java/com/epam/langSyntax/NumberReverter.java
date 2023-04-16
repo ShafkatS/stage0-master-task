be58e1447d9d830f3c936dbad21f1bf9abe124ed
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-16 10:30:49 +0500

    Update NumberReverter.java

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,19 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
+    int value = 0;
 
     public void revert(int number) {
+        while (number != 0) {
+            int expected = number % 10;
+            value = value * 10 + expected;
+            number /= 10;
+        }
+        System.out.println(value);
+    }
+    public static void main(String[] args) {
+        NumberReverter n = new NumberReverter();
+        n.reverter(123456789);
     }
 
 }
