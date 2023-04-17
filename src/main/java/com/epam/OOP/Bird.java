915a05e1e33641bf60035e31759c3b77894d9008
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 13:40:51 +0500

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,15 +1,11 @@
 package com.epam.OOP;
 
 public class Bird extends Animal {
-   public Bird() {
+  public Bird() {
+        super("blue",2,false);
     }
-    public Bird(String color, int numberOfPaws, boolean hasFur) {
-        super(color, numberOfPaws, hasFur);
-    }
-  
    @Override
-    public String getDescription() {
-        return "This animal is mostly " + getColor() + ". It has " + getNumberOfPaws() + " paws and no fur" + ". Moreover, it has 2 wings and can fly";
+    public String getDescription(){
+       return "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
     }
-
 }
