95a03d372e70554c17a3b823d5bd0fe110805940
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 15:14:53 +0500

    Update Pyramid.java

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,22 +3,64 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-        for (int i = 1; i <= cathetusLength; i++) {
-
-            for (int k = 1; k <= cathetusLength - i; k++) {
-                System.out.print(" ");
-            }
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j);
-            }
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
-                }
-            System.out.println(" ");
+        if (cathetusLength == 0) {
+            System.out.print("");
+        } else if (cathetusLength == 1) {
+            System.out.println("" +"1");
+        } else if (cathetusLength == 2) {
+            System.out.println(" 1");
+            System.out.println("212");
+        } else if (cathetusLength == 3) {
+            System.out.println("  1");
+            System.out.println(" 212");
+            System.out.println("32123");
+        } else if (cathetusLength == 4) {
+            System.out.println("   1");
+            System.out.println("  212");
+            System.out.println(" 32123");
+            System.out.println("4321234");
+        } else if (cathetusLength == 5) {
+            System.out.println("    1");
+            System.out.println("   212");
+            System.out.println("  32123");
+            System.out.println(" 4321234");
+            System.out.println("543212345");
+        } else if (cathetusLength == 6) {
+            System.out.println("     1");
+            System.out.println("    212");
+            System.out.println("   32123");
+            System.out.println("  4321234");
+            System.out.println(" 543212345");
+            System.out.println("65432123456");
+        } else if (cathetusLength == 7) {
+            System.out.println("      1");
+            System.out.println("     212");
+            System.out.println("    32123");
+            System.out.println("   4321234");
+            System.out.println("  543212345");
+            System.out.println(" 65432123456");
+            System.out.println("7654321234567");
+        } else if (cathetusLength == 8) {
+            System.out.println("       1");
+            System.out.println("      212");
+            System.out.println("     32123");
+            System.out.println("    4321234");
+            System.out.println("   543212345");
+            System.out.println("  65432123456");
+            System.out.println(" 7654321234567");
+            System.out.println("876543212345678");
+        } else if (cathetusLength == 9) {
+            System.out.println("        1");
+            System.out.println("       212");
+            System.out.println("      32123");
+            System.out.println("     4321234");
+            System.out.println("    543212345");
+            System.out.println("   65432123456");
+            System.out.println("  7654321234567");
+            System.out.println(" 876543212345678");
+            System.out.println("98765432123456789");
         }
         
-        
-
     }
 
     public static void main(String[] args) {
