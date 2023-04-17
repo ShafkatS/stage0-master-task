9fc138a739353bf5847dee7f8a0c264050787f21
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 13:41:22 +0500

    Update Dog.java

--- src/main/java/com/epam/OOP/Dog.java
+++ src/main/java/com/epam/OOP/Dog.java
@@ -1,10 +1,13 @@
 package com.epam.OOP;
 
 public class Dog extends  Animal{
-    public Dog() {
+ public Dog() {
+        super("brown",4,true);
     }
-    public Dog(String color, int numberOfPaws, boolean hasFur) {
-        super(color, numberOfPaws, hasFur);
+    @Override
+    public String getDescription(){
+        return "This animal is mostly brown. It has 4 paws and a fur.";
+
     }
 
 }
